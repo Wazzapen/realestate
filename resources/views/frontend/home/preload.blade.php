dbe08c9a3a08a4cc10b1126c54ff4fa84c34d0d9
Wazzapengit config --global user.email damanijordine@gmail.comgit config --global user.name Wazzapen <damanijordine@gmail.com>
2025-01-13 01:47:09 -0800

    firstv

---
+++ resources/views/frontend/home/preload.blade.php
@@ -0,0 +1,45 @@
+
+        <!-- preloader -->
+        <div class="loader-wrap">
+            <div class="preloader">
+                <div class="preloader-close"><i class="far fa-times"></i></div>
+                <div id="handle-preloader" class="handle-preloader">
+                    <div class="animation-preloader">
+                        <div class="spinner"></div>
+                        <div class="txt-loading">
+                            <span data-text-preloader="Y" class="letters-loading">
+                               Y
+                            </span>
+                            <span data-text-preloader="A" class="letters-loading">
+                                A
+                            </span>
+                            <span data-text-preloader="A" class="letters-loading">
+                                A
+                            </span>
+                            <span data-text-preloader="D" class="letters-loading">
+                                D
+                            </span>
+                            <span data-text-preloader="E" class="letters-loading">
+                               E
+                            </span>
+                            <span data-text-preloader="S" class="letters-loading">
+                                S
+                            </span>
+                            <span data-text-preloader="T" class="letters-loading">
+                                T
+                            </span>
+                            <span data-text-preloader="A" class="letters-loading">
+                                A
+                            </span>
+                            <span data-text-preloader="T" class="letters-loading">
+                                T
+                            </span>
+                            <span data-text-preloader="E" class="letters-loading">
+                                E
+                            </span>
+                        </div>
+                    </div>  
+                </div>
+            </div>
+        </div>
+        <!-- preloader end -->
